91c1bdbcdb6f0a9dddd420e654746c6b699d4de8
Alexander Naumov <alexander_naumov@opensuse.org>
2024-04-29 12:53:00 +0200

    install: git automake autoconf gmake

--- screen/build.sh
+++ screen/build.sh
@@ -3,6 +3,12 @@
 set -exu
 
 uname -a
+PATH="/usr/pkg/sbin:/usr/pkg/bin:$PATH"
+PKG_PATH="https://cdn.NetBSD.org/pub/pkgsrc/packages"
+PKG_PATH="$PKG_PATH/NetBSD/$(uname -p)/$(uname -r)/All/"
+#export PATH PKG_PATH
+pkg_add pkgin
+pkgin install git automake autoconf gmake
 git clone https://github.com/alexander-naumov/screen.git
 cd screen/src
 #export AUTOCONF_VERSION=2.71
