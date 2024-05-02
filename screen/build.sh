56a0b4817fa12ab3809b3a9704d9443cd30bbaa7
Alexander Naumov <alexander_naumov@opensuse.org>
2024-05-02 06:41:40 +0200

    update name of action + cleanup

--- screen/build.sh
+++ screen/build.sh
@@ -3,20 +3,10 @@
 set -exu
 
 uname -a
-#PATH="/usr/pkg/sbin:/usr/pkg/bin:$PATH"
-#PKG_PATH="https://cdn.NetBSD.org/pub/pkgsrc/packages"
-#PKG_PATH="$PKG_PATH/NetBSD/$(uname -p)/$(uname -r)/All/"
-#export PATH PKG_PATH
-#pkg_add pkgin
-#pkgin install git automake autoconf gmake
 git -c http.sslVerify=false clone https://github.com/alexander-naumov/screen.git
 cd screen/src
-#export AUTOCONF_VERSION=2.71
-#export AUTOMAKE_VERSION=1.15
 ./autogen.sh
-#echo "> ./configure --disable-pam CFLAGS='-Wall -DDEBUG'"
 ./configure CFLAGS="-Wall"
-#echo "> make"
 gmake
 ./screen -v
 ./screen --help
